03c6ad6acd45f7f85f6aedc5acc603dfae821d57
zhj <haijianzhang2020@163.com>
2026-04-11 23:16:47 +0800

    检查了网关模块的转发情况，正常

--- ai-gateway/src/main/java/com/zhj/learn/aigateway/AiGatewayApplication.java
+++ ai-gateway/src/main/java/com/zhj/learn/aigateway/AiGatewayApplication.java
@@ -1,12 +1,20 @@
 package com.zhj.learn.aigateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class AiGatewayApplication {
 
+    private static final Logger LOG= LoggerFactory.getLogger(AiGatewayApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(AiGatewayApplication.class, args);
+
+        SpringApplication app = new SpringApplication(AiGatewayApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("网关启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
     }
 }
